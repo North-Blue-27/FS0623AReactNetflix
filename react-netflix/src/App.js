cbbdfbc16196736cb0569fe704ed5316828bcacb
Casalena Antonio <casalenaantonio@hotmail.it>
2023-12-22 23:16:21 +0100

    modifiche

--- react-netflix/src/App.js
+++ react-netflix/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import MyNav from './components/MyNav';
@@ -7,7 +7,7 @@ import MovieGallery from './components/MovieGallery';
 
 function App() {
   const [searchTerm, setSearchTerm] = useState('');
-  
+  const [searchResults, setSearchResults] = useState([]);
 
   const handleSearch = (term) => {
     setSearchTerm(term);
@@ -17,18 +17,45 @@ function App() {
     setSearchTerm('');
   };
 
+  // Fetch search results when searchTerm changes
+  useEffect(() => {
+    const fetchData = async () => {
+      if (searchTerm) {
+        try {
+          const response = await fetch(`http://www.omdbapi.com/?apikey=65a18fc5&s=${searchTerm}`);
+          const data = await response.json();
+          if (data.Search) {
+            setSearchResults(data.Search);
+          }
+        } catch (error) {
+          console.error('Error fetching data:', error);
+        }
+      } else {
+        setSearchResults([]);
+      }
+    };
+
+    fetchData();
+  }, [searchTerm]);
+
+  // Static categories
+  const categories = [
+    { title: "Harry Potter", searchTerm: "harry potter" },
+    { title: "Matrix", searchTerm: "matrix" },
+    { title: "Star Wars", searchTerm: "star wars" }
+  ];
+
   return (
     <div className="App" style={{ backgroundColor: '#221f1f' }}>
       <MyNav onSearch={handleSearch} onHomeClick={handleHomeClick} />
-      {searchTerm ? (
+      {searchResults.length > 0 && (
         <MovieGallery title="Search Results" searchTerm={searchTerm} />
-      ) : (
-        <>
-          <MovieGallery title="Harry Potter" searchTerm="harry potter" />
-          <MovieGallery title="Matrix" searchTerm="matrix" />
-          <MovieGallery title="Star Wars" searchTerm="star wars" />
-        </>
       )}
+      <div className="categories">
+        {categories.map((category, index) => (
+          <MovieGallery key={index} title={category.title} searchTerm={category.searchTerm} />
+        ))}
+      </div>
       <MyFooter />
     </div>
   );
